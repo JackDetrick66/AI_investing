5e130a666dd5a1e0232342da5aa564205db28d31
Jack Detrick <98991308+JackDetrick66@users.noreply.github.com>
2025-12-07 18:50:06 -0500

    first

---
+++ AI_Investing/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Circular from './lib/circular_invest.svelte'
+</script>
+
+<main>
+  
+
+  <div class="card">
+    <Circular />
+  </div>
+</main>
+
+<style>
+  
+  
+</style>
---
+++ AI_Investing/src/lib/circular_invest.svelte
@@ -0,0 +1,45 @@
+
+<script>
+  import { onMount } from 'svelte';
+  import { Network } from 'vis-network';
+  
+  let container;
+
+  onMount(() => {
+
+    // create the array of nodes
+    const nodes = [
+    { id: 1, label: "NVIDIA"},
+    {id:2, label:"OPENAI"},
+    {id:3, label:"AMD"},
+    {id:4, label:"BROADCOM"},
+    {id:5, label:"COREWEAVE"}
+]
+
+// create an array of edges for the nodes
+const edges = [
+    {from: 1, to: 3}
+]
+
+
+
+const data = {nodes, edges};
+
+const options = {};
+
+const network = new Network(container, data, options);
+
+});
+</script>
+
+<div bind:this={container}></div>
+
+
+
+<style>
+    div{
+        width: 100%;
+        height: 100%;
+        border: 1px solid black;
+    }
+</style>
